865d49c4b443bb2345455e24f95b460f4c34511d
Anton Egorov <aegorov@aligntech.com>
2024-07-12 10:37:14 +0300

    MVP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-async function get_high_score() {
-  console.log("Handler: get_high_score");
+const dotenv = require('dotenv');
+
+const ydb_api = require('ydb_api');
+
+
+dotenv.config();
+const ydb_endpoint = process.env.YDB_ENDPOINT;
+const ydb_database_path = process.env.YDB_DATABASE_PATH;
+const ydb_table_name = process.env.YDB_TABLE_NAME;
+
+const ydb = new ydb_api(ydb_endpoint, ydb_database_path);
+
+
+async function get_max_id() {
+  const query = `SELECT MAX(id) as max FROM \`${ydb_table_name}\``;
+  const response = await ydb.query(query);
+
+  return response[0]['max'];
 }
 
-async function submit_score() {
-  console.log("Handler: submit_score");
+
+async function get_high_score() {
+  const query = `SELECT name, score, date FROM \`${ydb_table_name}\` ORDER BY score DESC LIMIT 10`;
+  const response = await ydb.query(query);
+
+  return response;
+}
+
+async function submit_score(name, score) {
+  const next_id = await get_max_id() + 1;
+
+  const query = `INSERT INTO \`${ydb_table_name}\` (id, name, score, date) VALUES (${next_id}, '${name}', ${score}, CurrentUtcDate())`;
+  await ydb.query(query);
+
+  return 'OK';
 }
 
 
--- src/test.js
+++ src/test.js
@@ -1,9 +1,56 @@
+const dotenv = require('dotenv');
+
+const ydb_api = require('ydb_api');
+
 const { get_high_score, submit_score } = require('./index.js');
 
 
+const test_name = 'Mr. Laggy';
+
+dotenv.config();
+const ydb_endpoint = process.env.YDB_ENDPOINT;
+const ydb_database_path = process.env.YDB_DATABASE_PATH;
+const ydb_table_name = process.env.YDB_TABLE_NAME;
+
+
 async function test() {
-  await get_high_score();
-  await submit_score();
+
+  console.log('-=-=-=-=-=-=- GET HIGH SCORE BEFORE -=-=-=-=-=--');
+
+  const high_score = await get_high_score();
+  console.log(high_score);
+  for (let i = 0; i < high_score.length; i++) {
+    console.log(`${high_score[i].name}, ${high_score[i].score}, ${high_score[i].date}`);
+  }
+
+  console.log('-=-=-=-=-=-=- SUBMIT SCORE -=-=-=-=-=--');
+
+  console.log(await submit_score(test_name, 24));
+
+  console.log('-=-=-=-=-=-=- GET HIGH SCORE AFTER -=-=-=-=-=--');
+
+  const high_score_after = await get_high_score();
+  console.log(high_score_after);
+  for (let i = 0; i < high_score_after.length; i++) {
+    console.log(`${high_score_after[i].name}, ${high_score_after[i].score}, ${high_score_after[i].date}`);
+  }
+
+  console.log('-=-=-=-=-=-=- CLEANUP -=-=-=-=-=--');
+
+  const ydb = new ydb_api(ydb_endpoint, ydb_database_path);
+  const query = `DELETE FROM \`${ydb_table_name}\` WHERE name = '${test_name}'`;
+  await ydb.query(query)
+  console.log('OK');
+
+  console.log('-=-=-=-=-=-=- GET HIGH SCORE AFTER CLEANUP -=-=-=-=-=--');
+
+  const high_score_after_cleanup = await get_high_score();
+  console.log(high_score_after_cleanup);
+  for (let i = 0; i < high_score_after_cleanup.length; i++) {
+    console.log(`${high_score_after_cleanup[i].name}, ${high_score_after_cleanup[i].score}, ${high_score_after_cleanup[i].date}`);
+  }
+
+  process.exit(0);
 }
 
 
